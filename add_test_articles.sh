6d5ce203adb0ac55eb3202829ba053e41661f72e
Suan Kan <kansuan@gmail.com>
2018-08-14 12:00:44 +1000

    Hid sample test json data into a separate directory

--- add_test_articles.sh
+++ add_test_articles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in 0 1 2 3; do
-    curl --data "@sample_article_$i.json" -H "Content-Type: application/json" http://127.0.0.1:5000/articles
+    curl --data "@sample_articles/sample_article_$i.json" -H "Content-Type: application/json" http://127.0.0.1:5000/articles
 done
